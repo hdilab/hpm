5e2eaa6a785a6328ac265899255c397d4980fe2f
Deokgun Park <intuinno@turing.uta.edu>
2020-05-08 16:07:05 -0500

    added a script

---
+++ exp14.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=1 python exp14.py data/short.txt -n EXP-14-short
+CUDA_VISIBLE_DEVICES=2 python exp14.py data/medium.txt -n EXP-14-medium
+CUDA_VISIBLE_DEVICES=3 python exp14.py data/1342.txt -n EXP-14-long
+
+
