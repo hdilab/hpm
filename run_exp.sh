94686fc7c5d19e9e118562e6d12e91d546ac903c
intuinno <intuinno@gmail.com>
2020-05-09 17:26:52 -0500

    HPM-EXP17 Re executing EXP15 with new text files and sigmoid

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0  python exp.py data/short.txt -n EXP-16-short -e 10000 -b 16384 &
-CUDA_VISIBLE_DEVICES=2 python exp.py data/medium.txt -n EXP-16-medium -e 10000 -b 16384  &
-CUDA_VISIBLE_DEVICES=3 python exp.py data/long.txt -n EXP-16-long -e 10000 -b 16384  &
+CUDA_VISIBLE_DEVICES=5  python exp.py data/short.txt -n EXP-17-short -e 10000 -b 163840 &
+CUDA_VISIBLE_DEVICES=8 python exp.py data/medium.txt -n EXP-17-medium -e 10000 -b 163840  &
+CUDA_VISIBLE_DEVICES=9 python exp.py data/long.txt -n EXP-17-long -e 10000 -b 163840  &
 
 
 
