636f834b930527c1676b7b37028fe1cf0988f066
intuinno <intuinno@gmail.com>
2020-05-12 13:45:41 -0500

    EXP-18 Two layer version of EXP-17

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=5  python exp.py data/100.txt -n EXP-17-100 -e 10000 -b 10 &
-CUDA_VISIBLE_DEVICES=8 python exp.py data/1k.txt -n EXP-17-1k -e 10000 -b 10  &
-CUDA_VISIBLE_DEVICES=9 python exp.py data/10k.txt -n EXP-17-10k -e 10000 -b 64  &
+CUDA_VISIBLE_DEVICES=2  python exp.py data/100.txt -n EXP-18-100 -e 10000 -b 10 &
+CUDA_VISIBLE_DEVICES=3 python exp.py data/1k.txt -n EXP-18-1k -e 10000 -b 10  &
+CUDA_VISIBLE_DEVICES=4 python exp.py data/10k.txt -n EXP-18-10k -e 10000 -b 64  &
 
 
 
