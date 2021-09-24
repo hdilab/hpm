169fd50e56e885fa0a1626ece455cc0fa2a2bf26
Deok Gun Park <intuinno@gmail.com>
2021-09-24 13:08:29 -0500

    EXP-27 - going home

--- models/AE.py
+++ models/AE.py
@@ -11,26 +11,30 @@ class autoencoder(nn.Module):
                  name='kWTA_AE',
                  numOnBits=10):
         super().__init__()
-
         self.numBits = numBits
         self.numOnBits = numOnBits
-        self.encoder = nn.Sequential(
-            nn.Linear(numBits*4, numBits*2),
-            nn.ReLU(True),
-            nn.Linear(numBits*2, numBits))
-        self.decoder = nn.Sequential(
-            nn.Linear(numBits, numBits*2),
-            nn.ReLU(True),
-            nn.Linear(numBits*2, numBits*4))
+        self.encoderL1 = nn.Linear(numBits*4, numBits*2)
+        self.encoderL2 = nn.Linear(numBits*2, numBits)
+        self.decoderL1 = nn.Linear(numBits, numBits*2)
+        self.decoderL2 = nn.Linear(numBits*2, numBits*4)
 
     def forward(self, x):
-        emb = self.encoder(x)
         k = self.numOnBits
-        tmpx = emb.view(x.shape[0], -1)
-        topval = tmpx.topk(k, dim=1)[0][:, -1]
-        topval = topval.repeat(tmpx.shape[1], 1).permute(1, 0).view_as(emb)
-        comp = (emb >= topval).to(emb)
-        sparseEmb = comp
-        x = self.decoder(sparseEmb)
-        return x, sparseEmb
+        L1out = self.encoderL1(x);
+        tmpL1 = L1out.view(x.shape[0],-1)
+        L1TopVal = tmpL1.topk(k * 2)[0][:, -1]
+        L1Sparse = (L1out > L1TopVal).to(L1out)
+
+        L2out = self.encoderL2(L1Sparse)
+        L2TopVal = L2out.topk(k * 1)[0][:, -1]
+        sparseEmb = (L2out > L2TopVal).to(L2out)
+
+        decoderL1out = self.decoderL1(sparseEmb);
+        decoderL1TopVal = decoderL1out.topk(k * 2)[0][:, -1]
+        decoderL1Sparse = (decoderL1out > decoderL1TopVal).to(decoderL1out)
+
+        decoderL2out = self.decoderL2(decoderL1Sparse)
+        decoderL2TopVal = decoderL2out.topk(k * 4)[0][:, -1]
+        out = (decoderL2out > decoderL2TopVal).to(decoderL2out)
+        return out, sparseEmb
 
