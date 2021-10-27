e6251d28b7a01aeb9f866f42498857a3fa3c7a14
Deokgun Park <intuinno@gmail.com>
2021-10-26 22:22:00 -0500

    Adding experiment shell

---
+++ run_exp39.sh
@@ -0,0 +1,15 @@
+python exp39.py  -n R4L1-10k   -p 10000 -l 1 -i data/10k.txt &
+python exp39.py  -n R4L1-100k  -p 10000 -l 1 -i data/100k.txt &
+python exp39.py  -n R4L1-1M    -p 10000 -l 1 -i data/1M.txt &
+python exp39.py  -n R4L2-10k   -p 10000 -l 2 -i data/10k.txt &
+python exp39.py  -n R4L2-100k  -p 10000 -l 2 -i data/100k.txt &
+python exp39.py  -n R4L2-1M    -p 10000 -l 2 -i data/1M.txt &
+python exp39.py  -n R4L3-10k   -p 10000 -l 3 -i data/10k.txt &
+python exp39.py  -n R4L3-100k  -p 10000 -l 3 -i data/100k.txt &
+python exp39.py  -n R4L3-1M    -p 10000 -l 3 -i data/1M.txt &
+python exp39.py  -n R4L4-10k   -p 10000 -l 4 -i data/10k.txt &
+python exp39.py  -n R4L4-100k  -p 10000 -l 4 -i data/100k.txt &
+python exp39.py  -n R4L4-1M    -p 10000 -l 4 -i data/1M.txt &
+python exp39.py  -n R4L5-10k   -p 10000 -l 5 -i data/10k.txt &
+python exp39.py  -n R4L5-100k  -p 10000 -l 5 -i data/100k.txt &
+python exp39.py  -n R4L5-1M    -p 10000 -l 5 -i data/1M.txt &
