fbb7b93761be3bf368ddca15f3aac95f59759034
Deokgun Park <intuinno@gmail.com>
2021-11-12 15:57:49 -0600

    Adding exp run shell

---
+++ run_exp43.sh
@@ -0,0 +1,6 @@
+python exp43.py  -n X1-10k  -p 10000 -l 5 -i data/10k.txt -x 1 &
+python exp43.py  -n X2-10k  -p 10000 -l 5 -i data/10k.txt -x 2 &
+python exp43.py  -n X4-10k  -p 10000 -l 5 -i data/10k.txt -x 4 &
+python exp43.py  -n X1-1M  -p 10000 -l 5 -i data/1M.txt -x 1 &
+python exp43.py  -n X2-1M  -p 10000 -l 5 -i data/1M.txt -x 2 &
+python exp43.py  -n X4-1M  -p 10000 -l 5 -i data/1M.txt -x 4 &
