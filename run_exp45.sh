2b34159727f3edb07e2fca0860fb5473074a8828
Deokgun Park <intuinno@wright.uta.edu>
2021-11-19 07:50:46 -0600

    Added runshell

---
+++ run_exp45.sh
@@ -0,0 +1,6 @@
+python exp45.py  -n TH10-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 10 &
+python exp45.py  -n TH12-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 12 &
+python exp45.py  -n TH14-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 14 &
+python exp45.py  -n TH16-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 16 &
+python exp45.py  -n TH18-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 18 &
+python exp45.py  -n TH20-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 20 &
