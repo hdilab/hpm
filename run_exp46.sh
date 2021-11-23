4946985330c629c2ca7f8d5819444ee2315460fe
Deokgun Park <intuinno@wright.uta.edu>
2021-11-22 21:05:15 -0600

    Added Runshell

---
+++ run_exp46.sh
@@ -0,0 +1,12 @@
+# python exp46.py  -n TH10-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 10 &
+# python exp46.py  -n TH12-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 12 &
+# python exp46.py  -n TH14-10k	-p 10000 -l 5 -i data/10k.txt -x 1 -r 14 &
+# python exp46.py  -n TH16-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 16 &
+# python exp46.py  -n TH18-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 18 &
+# python exp46.py  -n TH20-10k 	-p 10000 -l 5 -i data/10k.txt -x 1 -r 20 &
+# python exp46.py  -n TH10-1M 	-p 100000 -l 5 -i data/1M.txt -x 1 -r 10 &
+# python exp46.py  -n TH12-1M	  -p 100000 -l 5 -i data/1M.txt -x 1 -r 12 &
+# python exp46.py  -n TH14-1M 	-p 100000 -l 5 -i data/1M.txt -x 1 -r 14 &
+python exp46.py  -n TH04-1M 	-p 100000 -l 5 -i data/1M.txt -x 1 -r 4 &
+python exp46.py  -n TH08-1M 	-p 100000 -l 5 -i data/1M.txt -x 1 -r 8 &
+python exp46.py  -n TH06-1M 	-p 100000 -l 5 -i data/1M.txt -x 1 -r 6 &
